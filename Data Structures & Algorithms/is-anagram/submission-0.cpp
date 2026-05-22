911e7fee673beb15f90214bc3e4fa6dd83327ad9
Alex Michalak <atmichalak@gmail.com>
2026-05-22 19:39:49 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int char_count[26] = {0};
+
+        for (char c : s) {
+            char_count[c - 'a']++;
+        }
+
+        for (char c : t) {
+            char_count[c - 'a']--;
+
+            if (char_count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
